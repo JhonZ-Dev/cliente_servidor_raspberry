063cf7f5be256665a7413f23cc6d262213295303
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-12-01 22:07:21 -0500

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -4,3 +4,4 @@ host = '0.0.0.0'  # Escuchar en todas las interfaces de red
 port = 12345
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((host, port))
+server_socket.listen(5)  # Permitir hasta 5 conexiones simultáneas
