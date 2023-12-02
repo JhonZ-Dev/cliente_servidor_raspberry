ce0535e1f0c47aef2b48d485837c80cab6e7ae8b
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-12-01 22:07:04 -0500

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -2,3 +2,4 @@ import socket
 # Configuración del servidor
 host = '0.0.0.0'  # Escuchar en todas las interfaces de red
 port = 12345
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
