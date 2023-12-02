44a466eb8d820e8fc0166a7c463f81e710b5de44
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-12-01 22:07:13 -0500

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -3,3 +3,4 @@ import socket
 host = '0.0.0.0'  # Escuchar en todas las interfaces de red
 port = 12345
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((host, port))
